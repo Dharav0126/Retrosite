4b8c3ec8bc5e9d19ed57690ed2df42bd806fe326
Dharav <dharav2626@gmail.com>
2023-08-07 14:13:08 -0400

    first commit

---
+++ Script/script.js
@@ -0,0 +1,19 @@
+$(Document).ready(function () {
+    $(function () {
+        $('#head').load('meta.html');
+        $('#header').load('header.html');
+        $('#footer').load('footer.html');
+    });
+});
+
+// Functions show and hide login and signup form 
+function showLoginForm() {
+    document.getElementById('form').style.display = 'flex';
+    document.getElementById('subform').style.display = 'flex';
+}
+
+
+function closeForm() {
+    document.getElementById('form').style.display = 'none';
+    document.getElementById('subform').style.display = 'none';
+}

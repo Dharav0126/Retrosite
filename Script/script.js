ccad3d42fb6a1b63fb95a55c7710815c482bf3cf
Dharav <dharav2626@gmail.com>
2023-08-07 23:36:21 -0400

    responsive

--- Script/script.js
+++ Script/script.js
@@ -3,6 +3,7 @@ $(Document).ready(function () {
         $('#head').load('meta.html');
         $('#header').load('header.html');
         $('#footer').load('footer.html');
+        $('#aboutheader').load('header.html');
     });
 });
 
